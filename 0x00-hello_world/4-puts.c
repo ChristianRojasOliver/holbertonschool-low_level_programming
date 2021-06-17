54d41ecec13da8a1ee16e37b7d8c0527aa7f2bcc
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-06-17 18:00:19 +0000

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function puts print programing is like building a multilingual puzzel
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programing is like building a multilingual puzzle");
+
+	return (0);
+}
