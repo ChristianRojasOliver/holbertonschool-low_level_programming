110c7ebf85c246203c33d4c3d98916d9e2e65147
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-06-17 20:02:26 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,5 +7,9 @@
 int main(void)
 {
 	printf("Size of int = %ld", sizeof(int));
+	printf("Size of char = %ld", sizeof(char));
+	printf("Size of long int = %ld", sizeof(long int));
+	printf("Size of long long int = %ld", sizeof(long long int));
+	printf("Size of float = %ld", sizeof(float));
 	return (0);
 }
