dc5519fa4b7c879d498c815cc539518ae69523a5
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-06-18 23:30:20 +0000

    doble alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function print alphabetic order
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 97;
+	int A = 65;
+
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 90)
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
+}
