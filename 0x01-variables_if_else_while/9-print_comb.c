2aca5ea94546615adbb3c8f8ffc52e36ba8b8921
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-06-19 01:08:51 +0000

    all possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function print all posible combinations
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		if (a != 57)
+		putchar(',');
+		putchar(' ');
+		a++;
+	}
+	putchar('\n');
+		return (0);
+}
