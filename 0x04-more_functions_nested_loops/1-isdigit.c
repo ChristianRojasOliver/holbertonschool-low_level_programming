ee303474bade7a914abe6a870a9eef5f37925ff2
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-06-24 23:57:13 +0000

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - write a function that checks 0 to 9
+ * @c: character
+ * Return: 1 if c is a digit else return 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
