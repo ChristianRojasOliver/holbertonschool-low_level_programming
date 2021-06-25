17b31b9b9cef23b71b5cdb45eb5912cb91abc76f
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-06-25 00:18:55 +0000

    multipli 2 digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - write a function that multiplies two integers
+ * @a: Character
+ * @b: Character
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int num1;
+
+	num1 = a * b;
+	return (num1);
+}
