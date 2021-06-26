178cf0f85455f0fff75f0c0cdd53545522471e63
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-06-26 00:20:25 +0000

    count to 14 and the run 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b > 9)
+			{
+				_putchar(b / 10 + '0');
+			}
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
