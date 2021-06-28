89da9f28da3140c622fed2b8053f607859dbb751
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-06-28 20:10:44 +0000

    take pointer to an integer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - take an pointer to an int as parameter, value to 98
+ * @n: pointer
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
