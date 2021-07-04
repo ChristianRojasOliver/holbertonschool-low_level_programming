ddab0be633c5c914ad7a86d3d198fec68dd5ce87
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-07-04 15:02:14 +0000

    add two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  * int add(int, int); - add two integers and return the result
  * Return: always 0
  */
-int add(int num1, int num2)
+int add(int a, int b)
 {
-	return (num1 + num2);
+	return (a + b);
 }
