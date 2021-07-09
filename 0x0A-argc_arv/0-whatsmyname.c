466336f342c85e117ef52c47d9264bab3a157154
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-07-09 16:45:34 +0000

    print name

---
+++ 0x0A-argc_arv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print the name followed by \n
+ * Return: nothing
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
