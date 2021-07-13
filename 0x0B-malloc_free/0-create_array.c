f31a9471ac1eab60088125da0870efd9ffa5c0c6
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-07-13 15:45:33 +0000

    arrays of chars and initializes it with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - create an aaray of chars and initializes
+ * it with a specific char
+ * @size: parameter
+ * @c: parameter
+ * Return: allocate
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *allocate = NULL;
+	unsigned int counter;
+
+	allocate = malloc(sizeof(char) * 98);
+	if (allocate == NULL)
+	{
+		return (NULL);
+	}
+	for (counter = 0; counter < size; counter++)
+	{
+		allocate[counter] = c;
+	}
+	return (allocate);
+}
