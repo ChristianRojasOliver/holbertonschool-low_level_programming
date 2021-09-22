f23452957afa13dd15d23a35b12b9b11bda680f9
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-09-22 16:45:49 +0000

    fixing

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,264 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - absolute value of an integeri
+ * @n: character
+ * Return: 0
+ */
+int _abs(int n)
+{
+	return (0);
+}
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - function print lower & upper case characters
+ * @c: a character
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	return (0);
+}
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - function print lower case characters
+ * @c: a character
+ * Return: 0
+ */
+int _islower(int c)
+{
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @n: constant byte
+ * @src: parameter
+ * @dest: parameter
+ * Return: s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (0);
+}
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+#include "main.h"
+
+/**
+ * _strcat - function that concentrate two strings
+ * @dest: parameter
+ * @src: parameter
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (0);
+}
+
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: first string that you are going to compare
+ * @s2: secomd string that you are going to compare
+ * Return: s1
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * _strlen - calculates the string lenght of a variable
+ * @s: pointer
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * *_strncpy - function that copies a string
+ * @dest: parameter
+ * @src: parameter
+ * @n: integer
+ * Return: destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (0);
+}
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - get the lenght of a prefix substring
+ * @s: pointer
+ * @accept: pointer
+ * Return: idx2
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - function
+ * @s: parameter
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * _isdigit - write a function that checks 0 to 9
+ * @c: character
+ * Return: 1 if c is a digit else return 0
+ */
+int _isdigit(int c)
+{
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * _isupper - print a function that checks for uppercase character
+ * @c: Character
+ * Return: 1 if c is uppercase otherwise return 0
+ */
+int _isupper(int c)
+{
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * _memset - fill memory with a constant byte
+ * @n: constant byte
+ * @s: pointer
+ * @b: parameter
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * _puts - print a string follow by a new line
+ * @str: pointer
+ */
+void _puts(char *str)
+{
+	return;
+}
+
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: pointer
+ * @c: parameter
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * *_strcpy - copie the string pointed by src
+ * terminating null byte to the buffer pointed
+ * to by dest
+ * @src: pointer
+ * @dest: pointer
+ * Return: nothing
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * _strncat - function that concentrate two strings
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * _strpbrk - function that search a string for any of a set of bytes
+ * @s: pointer
+ * @accept: pointer
+ * Return: s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (0);
+}
+
+#include "main.h"
+
+/**
+ * _strstr - write a function that locates a substring
+ * @haystack: parameter
+ * @needle: parameter
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
