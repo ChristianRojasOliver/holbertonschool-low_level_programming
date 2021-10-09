3c49f4a7eaf257f4373c86714a33b9280e1352b8
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-10-09 03:20:34 +0000

    fixing betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,7 +3,7 @@
  * hash_table_get - retrieves a value associated with a key
  * @ht: is the hash table you want to look into
  * @key: is the key
- * Return : Returns the value associated with the element, or NULL
+ * Return: Returns the value associated with the element, or NULL
  * if key couldn't be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
