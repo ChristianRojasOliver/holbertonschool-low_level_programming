b1bb0294b704a104ff14aa4ba267c0beb44a5912
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-10-09 01:26:56 +0000

    fixing

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+/**
+* hash_table_set - gives you the index of a key
+* @ht: is the hash table you want to add or update the key/value to
+* @key: is the key
+* @value: value is the value associated with the key
+* Return:1 if it succeeded, 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node = NULL;
+	hash_node_t *aux_node = NULL;
+
+	if (!key || !value || !ht || strlen(key) == 0)
+	{
+		return (0);
+	}
+	index = key_index((unsigned char *)key, ht->size);
+	aux_node = (ht->array)[index];
+	while (aux_node != NULL)
+	{
+		if (strcmp(key, aux_node->key) == 0)
+		{
+			free(aux_node->value);
+			aux_node->value = strdup(value);
+			return (1);
+		}
+		aux_node = aux_node->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if ((ht->array)[index] == NULL)
+	{
+		(ht->array)[index] = new_node;
+	}
+	else
+	{
+		new_node->next = (ht->array)[index];
+		(ht->array)[index] = new_node;
+	}
+	return (1);
+}
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: is the hash table you want to look into
+ * @key: is the key
+ * Return : Returns the value associated with the element, or NULL
+ * if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index = 0;
+	hash_node_t *tmp;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	while (index < ht->size)
+	{
+		if (ht->array[index] != NULL)
+		{
+			tmp = ht->array[index];
+			while (tmp != NULL)
+			{
+				if (strcmp(key, tmp->key) == 0)
+					return (tmp->value);
+				tmp = tmp->next;
+			}
+		}
+		index++;
+	}
+	return (NULL);
+}
